edf3185857e59a2c1edfafd0d2d30ebe6a505628
Henry Donahue <henrydonahue@henrys-mbp-7.devices.brown.edu>
2022-12-07 11:28:41 -0500

    data

--- hw3/env_setup/conda_create_m1.sh
+++
@@ -1,9 +0,0 @@
-# conda remove -n "dl33"
-conda env create -n "dl35" -f csci1470-m1.yml
-
-## Install new environment.
-python -m ipykernel install --user --name dl335 --display-name "dl35"
-conda activate dl35
-
-## Tensorflow metal might malfunction for some students. Better to remove it
-pip uninstall tensorflow-metal
