f38e9dea4d2e7b989eb46feed5666b82e5083660
Henry Donahue <henrydonahue@henrys-mbp-2.mynetworksettings.com>
2022-12-09 18:35:06 -0500

    after meeting 2

--- hw3/code/assignment.py
+++ hw3/code/assignment.py
@@ -13,10 +13,7 @@ def preprocess_labels():
             split_line.remove(split_line[0])
             lines.append(np.array(split_line))
         final_return = np.vstack(lines)
-        #print(final_return)
         return final_return
-
-#preprocess_labels()
 
 
 def images_in_array(valueRange: list):
@@ -24,44 +21,10 @@ def images_in_array(valueRange: list):
 
     for f in valueRange:
         imagePath = image_folder_path + str(f)+ ".jpg"
-        image = imread(os.path.join(imagePath, f))
+        image = imread(imagePath)
         image = image.astype('float32') / 255.0
         image = tf.image.resize(image, [224,224])
         images.append(image)
 
     final_return = np.array(images)
-    
-    print(final_return.shape)
     return final_return
-
-
-    # for f in os.listdir(image_folder_path):
-    #     if f == '.DS_Store':
-    #         continue
-    #     image = imread(os.path.join(image_folder_path, f))
-    #     image = image.astype('float32') / 255.0
-    #     image = tf.image.resize(image, [224,224])
-    #     #print(image.shape)
-    #     images.append(image)
-
-    # final_return = np.array(images)
-    
-    # print(final_return.shape)
-    # return final_return
-    
-#images_in_array()
-
-
-# def divide_data():
-#     labels = preprocess_labels()
-#     images = images_in_array()
-    
-#     X0 = images
-#     Y0 = labels
-
-#     return X0, Y0
-
-
-# X0, Y0 = divide_data()
-# print(X0.shape)
-# print(Y0.shape)
